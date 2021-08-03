017aeb3a08673425319a1695641bd4db862ab201
wadekillua <wadekillua@gmail.com>
2021-08-03 13:56:32 +0800

    修改配置文件
    修改配置

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
+  title: 'Monsjer Blog',
+  author: 'Monsjer',
   email: 'i@craigary.net',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
